3b0ef824423c3d673c81e052deeb096b01ad2cc7
LorieFice <thib.magnin@orange.fr>
2024-02-23 10:50:22 +0100

    fonctionnel

---
+++ tp2/affiche_image/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Image aléatoire"),
+        ),
+        body: Center(
+          child: Image.network(
+            'https://picsum.photos/512/1024',
+            width: 512,
+            height: 1024,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
